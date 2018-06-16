57aef0ad3ba35c854bc9215ba0356907f7e3bbce
Zoldur <zoldur99@gmail.com>
2018-06-16 03:20:33 -0700

    Get IP

--- reliance_install.sh
+++ reliance_install.sh
@@ -13,7 +13,7 @@ COIN_PORT=43210
 RPC_PORT=43211
 
 
-NODEIP=$(curl -s4 icanhazip.com)
+NODEIP=$(curl -s4 api.ipify.org)
 
 RED='\033[0;31m'
 GREEN='\033[0;32m'
@@ -110,7 +110,7 @@ function get_ip() {
   declare -a NODE_IPS
   for ips in $(netstat -i | awk '!/Kernel|Iface|lo/ {print $1," "}')
   do
-    NODE_IPS+=($(curl --interface $ips --connect-timeout 2 -s4 icanhazip.com))
+    NODE_IPS+=($(curl --interface $ips --connect-timeout 2 -s4 api.ipify.org))
   done
 
   if [ ${#NODE_IPS[@]} -gt 1 ]
